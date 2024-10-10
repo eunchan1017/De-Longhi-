b25d988a85a53c124e9b0d93d2860250a7dab09f
eunchan1017 <jeongeunchan1017@gmail.com>
2024-10-10 15:54:26 +0900

    카테고리 호버효과 변경

--- js/main.js
+++ js/main.js
@@ -7,3 +7,16 @@ languageBtn.on("click", () => {
 $("#search-toggle").on("click", function () {
     $("#search-wrap").toggleClass("active"); // 검색창을 토글(보이기/숨기기)
 });
+const back = $(".back");
+const $submenu = $(".submenu");
+const $menu = $(".gnb > li ");
+const duration = 300; // 0.3초
+$menu.on("mouseenter", function () {
+    $(this).find($submenu).stop().addClass("on");
+    // $submenu.addClass("on");
+    back.addClass("on");
+});
+$menu.on("mouseleave", function () {
+    $submenu.removeClass("on");
+    back.removeClass("on");
+});
