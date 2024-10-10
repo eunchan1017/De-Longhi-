aaec16a5e0be3473213e3e6c3c74a9d083db3adb
eunchan1017 <jeongeunchan1017@gmail.com>
2024-10-10 13:11:59 +0900

    search click이벤트 추가

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ const languageSelect = $(".language-list");
 languageBtn.on("click", () => {
     languageSelect.toggleClass("on");
 });
+
+$("#search-toggle").on("click", function () {
+    $("#search-wrap").toggleClass("active"); // 검색창을 토글(보이기/숨기기)
+});
