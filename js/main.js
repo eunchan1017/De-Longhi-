03a75b466d8d2b136b4ea74c93c4e4877dd8a3c4
eunchan1017 <jeongeunchan1017@gmail.com>
2024-10-11 17:21:21 +0900

    임시 저장

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ const reviewList = new Swiper(".review-list", {
     // Optional parameters
     direction: "horizontal", //vertical
     loop: true,
-    autoplay: true,
+    autoplay: false,
     speed: 1000, //기본값 300
     effect: "slide", // slide, fade, cube, flip, coverflow
 
@@ -46,14 +46,14 @@ const reviewList = new Swiper(".review-list", {
     // },
     // If we need pagination
     pagination: {
-        el: ".swiper-pagination",
+        el: ".review-list-wrap .pagination",
         clickable: true,
     },
 
     // Navigation arrows
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".review-list-wrap .btn-next",
+        prevEl: ".review-list-wrap .btn-prev",
     },
 
     // And if we need scrollbar
@@ -61,3 +61,54 @@ const reviewList = new Swiper(".review-list", {
         el: ".swiper-scrollbar",
     },
 });
+const enChange = function (lang) {
+    // gnb 내용
+    $(".gnb > li:nth-child(1) > a").text("home");
+    $(".gnb > li:nth-child(2) > a").text("category");
+    $(".gnb > li:nth-child(3) > a").text("event");
+    $(".gnb > li:nth-child(4) > a").text("reviews");
+    $(".gnb > li:nth-child(5) > a").text("About as");
+
+    $(".visual-title h2").text("Experience the excellence of coffee");
+    $(".visual-title p").text("Discover the art of making De'Longhi's oriental coffee");
+    $(".visual-title a").text("buy now");
+
+    $(".category-list > li:nth-child(1) h4").text("Fully automatic");
+    $(".category-list > li:nth-child(2) h4").text("Semi automatic");
+    $(".category-list > li:nth-child(3) h4").text("electric kettle");
+    $(".category-list > li:nth-child(4) h4").text("cloud chino");
+    $(".category-list > li:nth-child(5) h4").text("air fryer");
+    $(".category-list > li:nth-child(6) h4").text("radiator");
+
+    $("#best-product .sec-title").text("Best Product");
+    $("#new-product .sec-title").text("New Product");
+    $("#reviews .sec-title").text("Reviews Product");
+
+    $(".member-wrap >li:nth-child(1) > a").html("<img src='./img/ico-language.png' alt='icon-language' />English");
+    $(".member-wrap >li:nth-child(2) > a").text("sign in");
+    $(".member-wrap >li:nth-child(3) > a").text("sign up");
+
+    $(".gnb li:nth-child(1) .submenu li:nth-child(1) a").text("Home");
+
+    $(".gnb li:nth-child(2) .submenu li:nth-child(1) a").text("Fully Coffee automatic");
+    $(".gnb li:nth-child(2) .submenu li:nth-child(2) a").text("Semi Coffee automatic");
+    $(".gnb li:nth-child(2) .submenu li:nth-child(3) a").text("Electronic kettle");
+    $(".gnb li:nth-child(2) .submenu li:nth-child(4) a").text("Radiator");
+
+    $(".gnb li:nth-child(3) .submenu li:nth-child(1) a").text("Event");
+
+    $(".gnb li:nth-child(4) .submenu li:nth-child(1) a").text("Coffee machine review");
+    $(".gnb li:nth-child(4) .submenu li:nth-child(2) a").text("Electric Kettle Reviews");
+    $(".gnb li:nth-child(4) .submenu li:nth-child(3) a").text("Other product reviews");
+
+    $(".gnb li:nth-child(5) .submenu li:nth-child(1) a").text("Directions");
+    $("#search-input").html();
+};
+
+$(".English").on("click", () => {
+    enChange("en");
+});
+$(".English").on("click", () => {
+    enChange("ko");
+});
+console.log($("#search-input").attr("placeholder", "검색어를 입력하세요"));
